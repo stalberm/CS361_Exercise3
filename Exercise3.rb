660c82135d9965ae6ac0a1771937e16fb38e71a7
Micah Stalberg <stalberm@oregonstate.edu>
2022-10-14 11:01:42 -0700

    Move max course var nearer to where its used per Bobs advice

--- Exercise3.rb
+++ Exercise3.rb
@@ -1,11 +1,10 @@
-class Student
-
-  MAX_COURSES = 5 
+class Student 
 
   def remove_from_schedule(course_list, course)
     course_list.remove(course) #Pass it only what it needs, the course list and the course to remove
   end
 
+  MAX_COURSES = 5
   def add_to_schedule(course_list, course)  #Pass it only what it needs, the course list and the course to add
     max_courses = course_list.maximum_number_of_courses
     if max_courses < MAX_COURSES
